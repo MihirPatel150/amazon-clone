a3c01f3a210c83500bfe398b8aad69e5495708b3
Mihir Patel <mihirpatel2082001@gmail.com>
2022-07-31 19:09:14 +0530

    Navbar added

---
+++ src/Header.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import logo from "./assets/amazon-white.png";
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <img className="header--logo" src={logo} alt="" />
+
+      <div className="header--search">
+        <input type="text" className="header--searchInput" />
+        <SearchIcon className="header--searchIcon" />
+      </div>
+
+      <div className="header--nav">
+        
+        <div className="header--option">
+          <span className="header--optionLineOne">Hello Guest</span>
+          <span className="header--optionLineTwo">Sign in</span>
+        </div>
+
+        <div className="header--option">
+          <span className="header--optionLineOne">Returns</span>
+          <span className="header--optionLineTwo">& Orders</span>
+        </div>
+        
+        <div className="header--option">
+          <span className="header--optionLineOne">Your</span>
+          <span className="header--optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header--optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header--optionLineTwo header--basketCount">0</span>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDA_qNhDhEBmIL9Zn3uN94B6t2R8_Y7XkY",
+    authDomain: "challenge-128ed.firebaseapp.com",
+    projectId: "challenge-128ed",
+    storageBucket: "challenge-128ed.appspot.com",
+    messagingSenderId: "1054260997507",
+    appId: "1:1054260997507:web:0e68920e8228a9b2ea9ab6",
+    measurementId: "G-54DE50LSQD"
+  };
